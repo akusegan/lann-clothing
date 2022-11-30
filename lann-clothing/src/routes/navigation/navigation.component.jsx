af3a2863986afd6f86fe63fb1c39b26be99f7889
Mazlan <akusegan@gmail.com>
2022-11-30 18:00:10 +0800

    connect to firebase

--- lann-clothing/src/routes/navigation/navigation.component.jsx
+++ lann-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,20 +1,27 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
 
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return (
       <Fragment>
         <div className="navigation">
           <Link className="logo-container" to='/'>
-            <div>Logo</div>
+            <CrownLogo className='logo'/>
           </Link>
           <div className="nav-links-container">
             <Link className="nav-link" to='/shop'>
               SHOP
             </Link>
+            <Link className="nav-link" to='/sign-in'>
+              SIGN IN
+            </Link>
           </div>
-          <Outlet/>
         </div>
+        <Outlet/>
       </Fragment>
     )
   }
---
+++ lann-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,71 @@
+import { initializeApp } from 'firebase/app';
+import { 
+    getAuth, 
+    signInWithRedirect, 
+    signInWithPopup, 
+    GoogleAuthProvider, 
+} from 'firebase/auth';
+import { 
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+ } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCK01-MsCGC5nAZpbP9GIn4C9xyR9Ndx1w",
+    authDomain: "lann-clothing-db.firebaseapp.com",
+    projectId: "lann-clothing-db",
+    storageBucket: "lann-clothing-db.appspot.com",
+    messagingSenderId: "353560699064",
+    appId: "1:353560699064:web:835cf48bbd70a046d2d44d"
+  };
+  
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();
+
+  provider.setCustomParameters({
+    prompt: 'select_account',
+  });
+
+  export const auth = getAuth();
+  export const singInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+            });
+        } catch (error){
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+    // if user daya does not exist
+    // create/set the document with the data from userAuth in my collection
+
+    //if user data exist
+
+
+    //return userDocRef
+  };
+
