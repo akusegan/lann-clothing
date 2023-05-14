15977190dd054f8f94aa677e6d1efae8628006df
Mazlan <akusegan@gmail.com>
2023-05-14 23:50:04 +0800

    [Styled-Component]-Buttons, Cart Dropdown

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import { singInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 import "./sign-in-form.styles.scss";
+import { GoogleSignInButton } from "../button/button.styles";
 
 const defaultFormFields = {
     email: '',
@@ -74,7 +75,7 @@ const SignUpForm = () => {
                 />
                 <div className="buttons-container">
                 <Button type='submit'>Sign In</Button>
-                <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+                <Button type='button' buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google Sign In</Button>
                 </div>
             </form>
         </div>
